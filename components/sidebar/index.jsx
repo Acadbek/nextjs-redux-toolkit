8ef9477bcdf3328fa4cf1e22e12fa6a3d5cda0dc
Asadbek <a.nosirjonov@uic.group>
2022-10-27 05:41:56 +0500

    feat: styles for posts page

--- components/sidebar/index.jsx
+++ components/sidebar/index.jsx
@@ -5,7 +5,7 @@ import icon from "../../public/icons/postsIcon.svg";
 const Sidebar = () => {
   return (
     <div className="bg-white px-3 h-full py-8">
-      <button className="bg-[#F5F6FA] w-full h-[40px] font-normal text-sm leading-4 text-black flex items-center justify-start py-[8px] px-4 gap-2">
+      <button className="bg-[#F5F6FA] rounded-lg w-full h-[40px] font-normal text-sm leading-4 text-black flex items-center justify-start py-[8px] px-4 gap-2">
         <Image src={icon} alt="" />
         <Link href={"/posts"}>Posts</Link>
       </button>
